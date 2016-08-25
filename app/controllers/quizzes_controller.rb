e2116ff9bb5faed95aa966a9c10fa1303c5462ea
cindy <jacobcindy@live.be>
2016-08-25 15:14:25 +0200

    Work3 in progress QuizzesController

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,13 +1,26 @@
 class QuizzesController < ApplicationController
+	before_action :get_quizz, only: [:edit, :update, :destroy]
+
+	def get_quizz
+		@quizz = Quizz.find(params[:id])
+	end 
  	
  	def index 
 		@quizz = Quizz.all
 	end 
 
 	def new
+		#authorize @quizz
+		@quizz = Quizz.new
 	end 
 
 	def create 
+		authorize @quizz
+		if @quizz = Quizz.create!(quizz_params)
+			redirect_to quizz_path(@question), :notice => t(:submit_flash)
+		else
+			redirect_to quizzes_path, :alert => "You can't do it"
+		end 
 	end 
 
 	def edit 
